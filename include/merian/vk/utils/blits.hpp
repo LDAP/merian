de5961b3c35d236228e0aa7d8f12a2d177d2e6c1
Lucas Alber <lucasd.alber@gmail.com>
2023-06-08 17:54:59 +0200

    Add blit helpers

---
+++ include/merian/vk/utils/blits.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "merian/vk/utils/math.hpp"
+#include "merian/vk/utils/subresource_ranges.hpp"
+#include "vulkan/vulkan.hpp"
+
+namespace merian {
+
+inline void cmd_blit_stretch(const vk::CommandBuffer& cmd,
+                             const vk::Image& src_image,
+                             const vk::ImageLayout& src_layout,
+                             const vk::Extent3D& src_extent,
+                             const vk::Image& dst_image,
+                             const vk::ImageLayout& dst_layout,
+                             const vk::Extent3D& dst_extent,
+                             const vk::Filter filter = vk::Filter::eLinear) {
+    vk::ImageBlit region{merian::first_layer(), {}, merian::first_layer(), {{}}};
+    region.srcOffsets[1] = extent_to_offset(src_extent);
+    region.dstOffsets[1] = extent_to_offset(dst_extent);
+    cmd.blitImage(src_image, src_layout, dst_image, dst_layout, {region}, filter);
+}
+
+// Scales down and centers the src image to fit the dst image. Can lead to borders.
+inline void cmd_blit_fit(const vk::CommandBuffer& cmd,
+                         const vk::Image& src_image,
+                         const vk::ImageLayout& src_layout,
+                         const vk::Extent3D& src_extent,
+                         const vk::Image& dst_image,
+                         const vk::ImageLayout& dst_layout,
+                         const vk::Extent3D& dst_extent,
+                         const vk::ClearColorValue clear_color = {},
+                         const vk::Filter filter = vk::Filter::eLinear) {
+    cmd.clearColorImage(dst_image, dst_layout, clear_color, {merian::all_levels_and_layers()});
+
+    vk::ImageBlit region{merian::first_layer(), {}, merian::first_layer(), {}};
+    region.srcOffsets[1] = extent_to_offset(src_extent);
+    float aspect = src_extent.width / (float)src_extent.height;
+    std::tie(region.dstOffsets[0], region.dstOffsets[1]) =
+        center(dst_extent, clamp_aspect(aspect, dst_extent));
+
+    cmd.blitImage(src_image, src_layout, dst_image, dst_layout, {region}, filter);
+}
+
+// Scales up and centers the source image. Can cut of parts of the source image.
+inline void cmd_blit_fill(const vk::CommandBuffer& cmd,
+                             const vk::Image& src_image,
+                             const vk::ImageLayout& src_layout,
+                             const vk::Extent3D& src_extent,
+                             const vk::Image& dst_image,
+                             const vk::ImageLayout& dst_layout,
+                             const vk::Extent3D& dst_extent,
+                             const vk::Filter filter = vk::Filter::eLinear) {
+    vk::ImageBlit region{merian::first_layer(), {}, merian::first_layer(), {}};
+    float aspect = dst_extent.width / (float)dst_extent.height;
+    std::tie(region.srcOffsets[0], region.srcOffsets[1]) =
+        center(src_extent, clamp_aspect(aspect, src_extent));
+    region.dstOffsets[1] = extent_to_offset(dst_extent);
+
+    cmd.blitImage(src_image, src_layout, dst_image, dst_layout, {region}, filter);
+}
+
+} // namespace merian
--- include/merian/vk/utils/math.hpp
+++ include/merian/vk/utils/math.hpp
@@ -10,4 +10,43 @@ inline vk::TransformMatrixKHR transform_identity() {
     return transform;
 }
 
+inline vk::Offset3D extent_to_offset(const vk::Extent3D& extent) {
+    return {(int32_t)extent.width, (int32_t)extent.height, (int32_t)extent.depth};
+}
+
+inline vk::Extent3D offset_to_extent(const vk::Offset3D& offset) {
+    return {(uint32_t)offset.x, (uint32_t)offset.y, (uint32_t)offset.z};
+}
+
+inline vk::Extent3D min(vk::Extent3D& a, vk::Extent3D& b) {
+    return {std::min(a.width, b.width), std::min(a.height, b.height), std::min(a.depth, b.depth)};
+}
+
+// Returns the largest extent with aspect that is smaller than `extent`.
+// Only width and height are considerd. For depth the original value is returned.
+inline vk::Extent3D clamp_aspect(float aspect, const vk::Extent3D& extent) {
+    const uint32_t width = std::min(extent.width, (uint32_t)(aspect * extent.height));
+    const uint32_t height = std::min(extent.height, (uint32_t)(extent.width / aspect));
+    return {width, height, extent.depth};
+}
+
+// Returns offsets that center region onto extent.
+inline std::pair<vk::Offset3D, vk::Offset3D> center(vk::Extent3D extent, vk::Extent3D region) {
+    assert(region.width <= extent.width);
+    assert(region.height <= extent.height);
+    assert(region.depth <= extent.depth);
+
+    const int32_t half_width_diff = (extent.width - region.width) / 2;
+    const int32_t half_height_diff = (extent.height - region.height) / 2;
+    const int32_t half_depth_diff = (extent.depth - region.depth) / 2;
+
+    const vk::Offset3D lower = {half_width_diff, half_height_diff, half_depth_diff};
+    const vk::Offset3D upper = {
+        (int32_t)extent.width - half_width_diff,
+        (int32_t)extent.height - half_height_diff,
+        (int32_t)extent.depth - half_depth_diff
+    };
+    return std::make_pair(lower, upper);
+}
+
 } // namespace merian
