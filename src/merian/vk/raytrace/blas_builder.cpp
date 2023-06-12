94c5646acb558977253fab2b370d761f5e4acd83
Lucas Alber <lucasd.alber@gmail.com>
2023-06-12 18:20:43 +0200

    BLAS: Update barriers

--- src/merian/vk/raytrace/blas_builder.cpp
+++ src/merian/vk/raytrace/blas_builder.cpp
@@ -93,33 +93,33 @@ void BLASBuilder::get_cmds(const vk::CommandBuffer& cmd, const EventHandle& sign
     ensure_scratch_buffer(pending_min_scratch_buffer);
     assert(scratch_buffer);
 
+    // Since the scratch buffer is reused across builds, we need a barrier to ensure one
+    // build is finished before starting the next one.
+    const vk::MemoryBarrier scratch_barrier{vk::AccessFlagBits::eAccelerationStructureReadKHR |
+                                  vk::AccessFlagBits::eAccelerationStructureWriteKHR,
+                              vk::AccessFlagBits::eAccelerationStructureReadKHR |
+                                  vk::AccessFlagBits::eAccelerationStructureWriteKHR};
     for (uint32_t idx = 0; idx < pending.size(); idx++) {
         pending[idx].build_info.scratchData.deviceAddress = scratch_buffer->get_device_address();
 
         cmd.buildAccelerationStructuresKHR(1, &pending[idx].build_info, pending[idx].range_info);
 
-        // Since the scratch buffer is reused across builds, we need a barrier to ensure one
-        // build is finished before starting the next one.
-        vk::MemoryBarrier barrier{vk::AccessFlagBits::eAccelerationStructureReadKHR |
-                                      vk::AccessFlagBits::eAccelerationStructureWriteKHR,
-                                  vk::AccessFlagBits::eAccelerationStructureReadKHR |
-                                      vk::AccessFlagBits::eAccelerationStructureWriteKHR};
         cmd.pipelineBarrier(vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR,
                             vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR, {}, 1,
-                            &barrier, 0, nullptr, 0, nullptr);
+                            &scratch_barrier, 0, nullptr, 0, nullptr);
     }
 
-    // Barrier for TLAS reads
-    vk::MemoryBarrier barrier{vk::AccessFlagBits::eAccelerationStructureReadKHR |
+    // Barrier for TLAS build / compaction reads
+    const vk::MemoryBarrier barrier{vk::AccessFlagBits::eAccelerationStructureReadKHR |
                                   vk::AccessFlagBits::eAccelerationStructureWriteKHR,
-                              vk::AccessFlagBits::eAccelerationStructureReadKHR |
-                                  vk::AccessFlagBits::eShaderRead};
+                              vk::AccessFlagBits::eAccelerationStructureReadKHR};
     cmd.pipelineBarrier(vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR,
                         vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR, {}, 1, &barrier,
                         0, nullptr, 0, nullptr);
 
     if (signal_event)
-        cmd.setEvent(signal_event->get_event(), vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR);
+        cmd.setEvent(signal_event->get_event(),
+                     vk::PipelineStageFlagBits::eAccelerationStructureBuildKHR);
 
     pending.clear();
     pending_min_scratch_buffer = 0;
