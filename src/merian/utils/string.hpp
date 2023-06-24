5afc9116a8b28106e137d3607cd6cd9afe753254
Lucas Alber <lucasd.alber@gmail.com>
2023-06-24 16:59:42 +0200

    Strings: Add utility for prefix and suffix

--- src/merian/utils/string.hpp
+++ src/merian/utils/string.hpp
@@ -8,15 +8,27 @@
 
 namespace merian {
 
-inline std::string format_size(const uint64_t size) {
+[[nodiscard]] inline std::string format_size(const uint64_t size) {
     std::vector<const char*> units = {"B", "KB", "MB", "GB", "TB"};
     std::size_t unit_index;
     if (size == 0) {
         unit_index = 0;
     } else {
-        unit_index = std::min((std::size_t) std::log2(size) / 10, units.size() - 1);
+        unit_index = std::min((std::size_t)std::log2(size) / 10, units.size() - 1);
     }
     return fmt::format("{} {}", size / std::pow(1024, unit_index), units[unit_index]);
+}
+
+[[nodiscard]] inline bool ends_with(const std::string& value, const std::string& suffix) {
+    if (suffix.size() > value.size())
+        return false;
+    return std::equal(suffix.rbegin(), suffix.rend(), value.rbegin());
+}
+
+[[nodiscard]] inline bool starts_with(const std::string& value, const std::string& prefix) {
+    if (prefix.size() > value.size())
+        return false;
+    return std::equal(prefix.begin(), prefix.end(), value.begin());
 }
 
 } // namespace merian
