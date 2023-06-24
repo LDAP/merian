fc31b84af3fc90621ef5d309c222961c7dc7e034
Lucas Alber <lucasd.alber@gmail.com>
2023-06-24 16:59:30 +0200

    Math: Add fit function

--- src/merian/vk/utils/math.hpp
+++ src/merian/vk/utils/math.hpp
@@ -7,34 +7,35 @@
 
 namespace merian {
 
-inline vk::TransformMatrixKHR transform_identity() {
+inline vk::TransformMatrixKHR transform_identity() noexcept {
     vk::TransformMatrixKHR transform;
     transform.matrix[0][0] = transform.matrix[1][1] = transform.matrix[2][2] = 1.0f;
     return transform;
 }
 
-inline vk::Offset3D extent_to_offset(const vk::Extent3D& extent) {
+inline vk::Offset3D extent_to_offset(const vk::Extent3D& extent) noexcept {
     return {(int32_t)extent.width, (int32_t)extent.height, (int32_t)extent.depth};
 }
 
-inline vk::Extent3D offset_to_extent(const vk::Offset3D& offset) {
+inline vk::Extent3D offset_to_extent(const vk::Offset3D& offset) noexcept {
     return {(uint32_t)offset.x, (uint32_t)offset.y, (uint32_t)offset.z};
 }
 
-inline vk::Extent3D min(vk::Extent3D& a, vk::Extent3D& b) {
+inline vk::Extent3D min(const vk::Extent3D& a, const vk::Extent3D& b) noexcept {
     return {std::min(a.width, b.width), std::min(a.height, b.height), std::min(a.depth, b.depth)};
 }
 
-// Returns the largest extent with aspect that is smaller than `extent`.
-// Only width and height are considerd. For depth the original value is returned.
-inline vk::Extent3D clamp_aspect(float aspect, const vk::Extent3D& extent) {
-    const uint32_t width = std::min(extent.width, (uint32_t)(aspect * extent.height));
-    const uint32_t height = std::min(extent.height, (uint32_t)(extent.width / aspect));
-    return {width, height, extent.depth};
+inline vk::Extent3D add(const vk::Extent3D& a, const vk::Extent3D& b) noexcept {
+    return {a.width + b.width, a.height + b.height, a.depth + b.depth};
+}
+
+inline vk::Offset3D add(const vk::Offset3D& a, const vk::Offset3D& b) noexcept {
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
 // Returns offsets that center region onto extent.
-inline std::pair<vk::Offset3D, vk::Offset3D> center(vk::Extent3D extent, vk::Extent3D region) {
+inline std::pair<vk::Offset3D, vk::Offset3D> center(const vk::Extent3D& extent,
+                                                    const vk::Extent3D& region) noexcept {
     assert(region.width <= extent.width);
     assert(region.height <= extent.height);
     assert(region.depth <= extent.depth);
@@ -50,13 +51,39 @@ inline std::pair<vk::Offset3D, vk::Offset3D> center(vk::Extent3D extent, vk::Ext
     return std::make_pair(lower, upper);
 }
 
+// Fits src into dst and returns the new dst offsets.
+inline std::pair<vk::Offset3D, vk::Offset3D> fit(const vk::Offset3D& src_lower,
+                                                 const vk::Offset3D& src_upper,
+                                                 const vk::Offset3D& dst_lower,
+                                                 const vk::Offset3D& dst_upper) noexcept {
+    const int32_t src_dx = src_upper.x - src_lower.x;
+    const int32_t src_dy = src_upper.y - src_lower.y;
+    const int32_t src_dz = src_upper.z - src_lower.z;
+
+    const int32_t dst_dx = dst_upper.x - dst_lower.x;
+    const int32_t dst_dy = dst_upper.y - dst_lower.y;
+    const int32_t dst_dz = dst_upper.z - dst_lower.z;
+
+    assert(src_dx > 0); assert(src_dy > 0); assert(src_dz > 0);
+    assert(dst_dx > 0); assert(dst_dy > 0); assert(dst_dz > 0);
+
+    const float scale =
+        std::min(std::min(dst_dx / (float)src_dx, dst_dy / (float)src_dy), dst_dz / (float)src_dz);
+    const auto [ctr_lower, ctr_upper] =
+        center(vk::Extent3D(dst_dx, dst_dy, dst_dz),
+               vk::Extent3D(std::round(src_dx * scale), std::round(src_dy * scale),
+                            std::round(src_dz * scale)));
+
+    return std::make_pair(add(dst_lower, ctr_lower), add(dst_lower, ctr_upper));
+}
+
 // Rotate "pos" around "origin". right-left (phi), up-down(theta).
 // Keeps the up direction valid.
 inline void rotate_around(glm::vec3& pos,
                           const glm::vec3& origin,
                           const glm::vec3& up,
                           const float d_phi,
-                          const float d_theta) {
+                          const float d_theta) noexcept {
     const glm::vec3 origin_to_pos(pos - origin);
     const glm::vec3 normalized_origin_to_pos = glm::normalize(origin_to_pos);
 
@@ -81,12 +108,12 @@ inline void rotate_around(glm::vec3& pos,
 }
 
 // Calculates the lowest common multiple of two numbers
-inline uint32_t lcm(uint32_t a, uint32_t b) {
+inline uint32_t lcm(uint32_t a, uint32_t b) noexcept {
     return (a * b) / std::gcd(a, b);
 }
 
 // Calculates the lowest common multiple of numbers
-inline uint32_t lcm(std::vector<uint32_t> numbers) {
+inline uint32_t lcm(std::vector<uint32_t> numbers) noexcept {
     if (numbers.empty())
         return 0;
     if (numbers.size() == 1) {
