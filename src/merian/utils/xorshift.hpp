bf05ebd9640a21ced6c97263a326c8931fc7d204
Lucas Alber <lucasd.alber@gmail.com>
2023-06-28 10:53:16 +0200

    XorShift: Format

--- src/merian/utils/xorshift.hpp
+++ src/merian/utils/xorshift.hpp
@@ -2,12 +2,14 @@
 
 #include <cstdint>
 
+namespace merian {
+
 // Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs"
 class XORShift32 {
   public:
     XORShift32(uint32_t seed) : state(seed) {}
 
-    // returns a double 
+    // returns a double
     double get() {
         state ^= state << 13;
         state ^= state >> 17;
@@ -18,3 +20,5 @@ class XORShift32 {
   private:
     uint32_t state;
 };
+
+} // namespace merian
