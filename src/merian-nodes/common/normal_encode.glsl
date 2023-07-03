897a335927f781612a46d6dab52e991301ac0c81
Lucas Alber <lucasd.alber@gmail.com>
2023-07-03 11:13:08 +0200

    merian-nodes: Add some `const` to variables

--- src/merian-nodes/common/normal_encode.glsl
+++ src/merian-nodes/common/normal_encode.glsl
@@ -1,7 +1,7 @@
 #ifndef _NORMAL_ENCODE_H_
 #define _NORMAL_ENCODE_H_
 
-vec2 sign_no0(vec2 v)
+vec2 sign_no0(const vec2 v)
 {
   return mix(vec2(1.0), vec2(-1.0), lessThan(v, vec2(0.0)));
 }
@@ -11,7 +11,7 @@ vec2 sign_no0(vec2 v)
 // almost like oct30
 vec3 geo_decode_normal(const uint enc)
 {
-  vec2 projected = unpackSnorm2x16(enc); // -1..1
+  const vec2 projected = unpackSnorm2x16(enc); // -1..1
   vec3 vec = vec3(projected, 1.0-abs(projected.x)-abs(projected.y));
   if(vec.z < 0.0) vec.xy = vec2(1.0-abs(vec.yx)) * sign_no0(vec.xy);
   return normalize(vec);
@@ -20,7 +20,7 @@ vec3 geo_decode_normal(const uint enc)
 // 32-bit normal encoding from Journal of Computer Graphics Techniques Vol. 3, No. 2, 2014
 // A Survey of Efficient Representations for Independent Unit Vectors,
 // almost like oct30
-uint geo_encode_normal(vec3 n)
+uint geo_encode_normal(const vec3 n)
 {
   const float invL1Norm = 1.0 / (abs(n.x) + abs(n.y) + abs(n.z));
   vec2 enc; // find floating point values of octahedral map in [-1,1]:
--- src/merian-nodes/common/von_mises_fisher.glsl
+++ src/merian-nodes/common/von_mises_fisher.glsl
@@ -4,20 +4,20 @@
 #define _VON_MISES_FISHER_H_
 
 // numerically robust von Mises Fisher lobe
-float vmf_eval(float kappa, float dotmu)
+float vmf_eval(const float kappa, const float dotmu)
 {
   if(kappa < 1e-4) return 1.0/(4.0*M_PI);
   return kappa/(2.0*M_PI*(1.0 - exp(-2.0*kappa))) * exp(kappa*(dotmu-1.0));
 }
 
-vec3 vmf_sample(float kappa, vec2 r)
+vec3 vmf_sample(const float kappa, const vec2 r)
 { // see wenzel's doc on numerically stable expression for vmm:
   float w = 1.0 + log(r.x + (1.0-r.x)*exp(-2.0*kappa))/kappa;
   vec2 v = vec2(sin(2.0*M_PI*r.y), cos(2.0*M_PI*r.y));
   return vec3(sqrt(1.0-w*w)*v, w);
 }
 
-float vmf_get_kappa(float x)
+float vmf_get_kappa(const float x)
 { // compute concentration parameter for given maximum density x
   if(x > 0.795) return 2.0*M_PI*x;
   return max(1e-5, (168.479*x*x + 16.4585*x - 2.39942)/
