f07549e8fb86b3ac85e9670c822ff9f597684747
Lucas Alber <lucasd.alber@gmail.com>
2023-07-03 11:12:33 +0200

    vector utils: Add function to copy raw data

--- src/merian/utils/vector.hpp
+++ src/merian/utils/vector.hpp
@@ -10,6 +10,13 @@ template <class T> void insert_all(std::vector<T>& to, std::vector<T> const& fro
     std::copy(std::begin(from), std::end(from), std::back_inserter(to));
 }
 
+// Copies the memory from `from` to the back of `to`. `to` is accordingly resized.
+template <class T> void raw_copy_back(std::vector<T>& to, std::vector<T> const& from) {
+    std::size_t old_size = to.size();
+    to.resize(old_size + from.size());
+    memcpy(&to[old_size], from.data(), from.size() * sizeof(T));
+}
+
 template <class T> void check_size(const std::vector<T>& vector, std::size_t index) {
     if (index >= vector.size()) {
         throw std::runtime_error(fmt::format("Index {} invalid for size {}", index, vector.size()));
