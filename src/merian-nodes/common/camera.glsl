f11abc6a29e4452cef1274cf2314fcfb5b44d9d3
Lucas Alber <lucasd.alber@gmail.com>
2023-07-05 13:16:30 +0200

    merian-nodes: Common: Add camera with halton points

--- src/merian-nodes/common/camera.glsl
+++ src/merian-nodes/common/camera.glsl
@@ -13,11 +13,31 @@ vec2 filter_bh_sample(const vec2 rand) {
 
 // Returns a camera ray direction for a pixel. Expects up and forward to be normalized.
 // The direction is randomized using a blackman harris pixel filter.
-vec3 get_camera_ray_dir(const ivec2 pixel, const ivec2 resolution, const vec3 up, const vec3 forward, const vec4 rand) {
+vec3 get_camera_ray_dir_bh(const ivec2 pixel, const ivec2 resolution, const vec3 up, const vec3 forward, const vec2 rand) {
     const vec3 t = -up * float(resolution.y) / float(resolution.x);
     const vec3 r = -normalize(cross(forward, t));
-    const vec2 off = filter_bh_sample(rand.yz);
+    const vec2 off = filter_bh_sample(rand);
     const vec2 uv = (pixel + off) / resolution - 0.5;
+    return normalize(0.45 * forward + r * uv.x + t * uv.y);
+}
+
+// Returns a camera ray direction for a pixel. Expects up and forward to be normalized.
+// The direction is offset by a halton sequence of lenght 8.
+vec3 get_camera_ray_dir_halton(const ivec2 pixel, const ivec2 resolution, const vec3 up, const vec3 forward, const uint frame) {
+    const vec2 halton_sequence[8] = {
+        vec2(1.0 /  2.0, 1.0 / 3.0),
+        vec2(1.0 /  4.0, 2.0 / 3.0),
+        vec2(3.0 /  4.0, 1.0 / 9.0),
+        vec2(1.0 /  8.0, 4.0 / 9.0),
+        vec2(5.0 /  8.0, 7.0 / 9.0),
+        vec2(3.0 /  8.0, 2.0 / 9.0),
+        vec2(7.0 /  8.0, 5.0 / 9.0),
+        vec2(1.0 / 16.0, 8.0 / 9.0)
+    };
+
+    const vec3 t = -up * float(resolution.y) / float(resolution.x);
+    const vec3 r = -normalize(cross(forward, t));
+    const vec2 uv = (pixel + halton_sequence[frame & 7]) / resolution - 0.5;
     return normalize(0.45 * forward + r * uv.x + t * uv.y);
 }
 
