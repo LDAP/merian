6093391ce67f5541573ea8061ea0930cbfe77ef2
Lucas Alber <lucasd.alber@gmail.com>
2023-07-06 23:39:48 +0200

    Raytrace: go back to instance ids

--- src/merian-nodes/common/raytrace.glsl
+++ src/merian-nodes/common/raytrace.glsl
@@ -2,7 +2,7 @@
 #define _RAYTRACE_H_
 
 struct IntersectionInfo {
-    uint instance_custom_index;
+    uint instance_id;
     // primitive index in the instance
     uint primitive_index;
     vec3 barycentrics;
@@ -12,7 +12,7 @@ struct IntersectionInfo {
 
 // The the intersection info for a commited intersection
 void get_intersection_info_commited(const rayQueryEXT ray_query, inout IntersectionInfo info) {
-    info.instance_custom_index = rayQueryGetIntersectionInstanceCustomIndexEXT(ray_query, true);
+    info.instance_id = rayQueryGetIntersectionInstanceIdEXT(ray_query, true);
     info.primitive_index = rayQueryGetIntersectionPrimitiveIndexEXT(ray_query, true);
     info.barycentrics.yz = rayQueryGetIntersectionBarycentricsEXT(ray_query, true);
     info.barycentrics.x = 1.0 - info.barycentrics.z - info.barycentrics.y;
@@ -21,7 +21,7 @@ void get_intersection_info_commited(const rayQueryEXT ray_query, inout Intersect
 
 // The the intersection info for an uncommited intersection
 void get_intersection_info_uncommited(const rayQueryEXT ray_query, inout IntersectionInfo info) {
-    info.instance_custom_index = rayQueryGetIntersectionInstanceCustomIndexEXT(ray_query, false);
+    info.instance_id = rayQueryGetIntersectionInstanceIdEXT(ray_query, false);
     info.primitive_index = rayQueryGetIntersectionPrimitiveIndexEXT(ray_query, false);
     info.barycentrics.yz = rayQueryGetIntersectionBarycentricsEXT(ray_query, false);
     info.barycentrics.x = 1.0 - info.barycentrics.z - info.barycentrics.y;
