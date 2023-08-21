f389b8bd3a6ac1c89333b8e13b039e3b0dd59d1c
Lucas Alber <lucasd.alber@gmail.com>
2023-08-21 12:52:06 +0200

    math: Add yuv luminance function

--- src/merian/utils/colors.hpp
+++ src/merian/utils/colors.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "glm/glm.hpp"
+
 #include <cmath>
 #include <cstdint>
 
@@ -9,5 +11,9 @@ inline uint32_t uint32_from_rgba(float r, float g, float b, float a) {
     return uint32_t(std::round(r * 255)) | (uint32_t(std::round(g * 255)) << 8) |
            uint32_t(std::round(b * 255)) << 16 | uint32_t(std::round(a * 255)) << 24;
 };
+
+inline float yuv_luminance(glm::vec3 color) {
+    return glm::dot(color, glm::vec3(0.299, 0.587, 0.114));
+}
 
 } // namespace merian
