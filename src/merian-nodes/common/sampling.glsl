9d69febcd78771622d2f3de936156c5d0fb1e2fa
Lucas Alber <lucasd.alber@gmail.com>
2023-08-23 09:52:20 +0200

    merian-nodes: sampling: Add hemisphere sample

--- src/merian-nodes/common/sampling.glsl
+++ src/merian-nodes/common/sampling.glsl
@@ -3,11 +3,17 @@
 #ifndef _SAMPLING_H_
 #define _SAMPLING_H_
 
-// sample sphere, p = 1/4pi
+// sample sphere, p = 1 / 4pi
 vec3 sample_sphere(const vec2 random) {
    const float z = 2.0 * (random.x - 0.5);
    const float z2 = sqrt(1.0 - z * z);
    return vec3(z2 * cos(2.0 * M_PI * random.y), z2 * sin(2.0 * M_PI * random.y), z);
+}
+
+// sample hemisphere (around 0,0,1), p = 1 / 2pi
+vec3 sample_hemisphere(const vec2 random) {
+   const float su = sqrt(1 - (1 - random.x) * (1 - random.x));
+   return vec3(su * cos(2.0 * M_PI * random.y), su * sin(2.0 * M_PI * random.y), 1 - random.x);
 }
 
 // sample hemisphere (around 0,0,1), cos lobe, p = cos(theta) / pi
