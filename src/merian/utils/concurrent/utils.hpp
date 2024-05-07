84b194c15c87fe9efb6241d5da2da8c4a695bc4e
Lucas Alber <lucasd.alber@gmail.com>
2024-05-07 19:52:29 +0200

    concurrent utils: Allow setting the number of tasks

--- src/merian/utils/concurrent/utils.hpp
+++ src/merian/utils/concurrent/utils.hpp
@@ -8,16 +8,18 @@
 
 namespace merian {
 
-// Run a function `count` times. The function get the index [0,count) and the thread index
+// Run a function `count` times split into 'tasks' tasks.
+// The function gets the index [0,count) and the task index
 // [0,concurrency).
 inline void parallel_for(const uint32_t count,
                          const std::function<void(uint32_t index, uint32_t thread_index)> function,
-                         ThreadPool& thread_pool) {
+                         ThreadPool& thread_pool,
+                         const uint32_t tasks = std::thread::hardware_concurrency()) {
     if (count == 0)
         return;
 
     std::vector<std::future<void>> futures;
-    uint32_t real_concurrency = std::min(count, thread_pool.size());
+    uint32_t real_concurrency = std::min(count, tasks);
     uint32_t count_per_thread = (count + real_concurrency - 1) / real_concurrency;
 
     for (uint32_t thread_index = 0; thread_index < real_concurrency; thread_index++) {
@@ -35,13 +37,14 @@ inline void parallel_for(const uint32_t count,
     }
 }
 
-// Run a function `count` times. The function get the index [0,count) and the thread index
+// Run a function `count` times split into 'tasks' tasks.
+// The function gets the index [0,count) and the task index
 // [0,concurrency).
 inline void parallel_for(const uint32_t count,
                          const std::function<void(uint32_t index, uint32_t thread_index)> function,
-                         const uint32_t concurrency = std::thread::hardware_concurrency()) {
-    ThreadPool thread_pool(concurrency);
-    parallel_for(count, function, thread_pool);
+                         const uint32_t tasks = std::thread::hardware_concurrency()) {
+    ThreadPool thread_pool(std::thread::hardware_concurrency());
+    parallel_for(count, function, thread_pool, tasks);
 }
 
 } // namespace merian
