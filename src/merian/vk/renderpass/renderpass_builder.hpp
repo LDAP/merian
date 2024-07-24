7a3824748cbbc92162fe09f6eaaec9af46c52c76
Lucas Alber <lucasd.alber@gmail.com>
2024-07-24 15:19:57 +0200

    merian: Add renderpass builder

--- src/merian/vk/renderpass/renderpass_builder.hpp
+++ src/merian/vk/renderpass/renderpass_builder.hpp
@@ -8,14 +8,15 @@ class RenderpassBuilder {
   public:
     using AttachmentHandle = uint32_t;
     using SubpassHandle = uint32_t;
+    static const AttachmentHandle NULL_ATTACHMENT_HANDLE = ~0;
 
     RenderpassBuilder(
+        vk::PipelineBindPoint first_subpass_pipeline_bind_point = vk::PipelineBindPoint::eGraphics,
         const vk::SubpassDescriptionFlags first_subpass_flags = {},
-        vk::PipelineBindPoint first_subpass_pipeline_bind_point = vk::PipelineBindPoint::eGraphics);
+        const vk::RenderPassCreateFlags renderpass_create_flags = {});
 
     // --- 1. Set attachments ---
 
-    // returns the attachment index
     [[nodiscard]]
     AttachmentHandle
     add_attachment(const vk::ImageLayout initial_layout = vk::ImageLayout::eUndefined,
@@ -31,29 +32,30 @@ class RenderpassBuilder {
     // --- 2. Configure subpasses ---
 
     [[nodiscard]]
-    SubpassHandle current_subpass();
+    SubpassHandle first_subpass() const;
 
-    // Starts a new subpass and returns it handle
     [[nodiscard]]
-    SubpassHandle
-    next_subpass(const vk::SubpassDescriptionFlags subpass_flags,
-                 vk::PipelineBindPoint pipelineBindPoint = vk::PipelineBindPoint::eGraphics);
+    SubpassHandle current_subpass() const;
+
+    SubpassHandle next_subpass(
+        const vk::PipelineBindPoint subpass_pipeline_bind_point = vk::PipelineBindPoint::eGraphics,
+        const vk::SubpassDescriptionFlags subpass_flags = {});
 
     RenderpassBuilder&
     add_input_attachment(const AttachmentHandle& attachment,
-                         vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
+                         const vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
 
-    RenderpassBuilder&
-    add_color_attachment(const AttachmentHandle& attachment,
-                         vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
+    RenderpassBuilder& add_color_attachment(
+        const AttachmentHandle& attachment,
+        const vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal,
+        const AttachmentHandle& resolve_attachment = NULL_ATTACHMENT_HANDLE,
+        const vk::ImageLayout resolve_layout = vk::ImageLayout::eColorAttachmentOptimal);
 
-    RenderpassBuilder&
-    add_resolve_attachment(const AttachmentHandle& attachment,
-                           vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
+    RenderpassBuilder& add_preserve_attachment(const AttachmentHandle& attachment);
 
-    RenderpassBuilder&
-    set_depth_stencil_attachment(const AttachmentHandle& attachment,
-                                 vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
+    RenderpassBuilder& set_depth_stencil_attachment(
+        const AttachmentHandle& attachment,
+        const vk::ImageLayout layout = vk::ImageLayout::eColorAttachmentOptimal);
 
     // --- 3. Set subpass dependencies  ---
 
@@ -65,27 +67,26 @@ class RenderpassBuilder {
         const vk::AccessFlags src_access_flags = vk::AccessFlagBits::eMemoryRead |
                                                  vk::AccessFlagBits::eMemoryWrite,
         const vk::AccessFlags dst_access_flags = vk::AccessFlagBits::eMemoryRead |
-                                                 vk::AccessFlagBits::eMemoryWrite);
+                                                 vk::AccessFlagBits::eMemoryWrite,
+        const vk::DependencyFlags dependency_flags = {});
 
     // --- 4. Build ---
 
     [[nodiscard]]
     RenderPassHandle build(const ContextHandle& context);
 
-    // Resets the builder to build a new RenderPass
-    void clear();
-
   private:
-    vk::RenderPassCreateFlags renderpass_flags;
+    vk::RenderPassCreateFlags renderpass_create_flags;
     std::vector<vk::AttachmentDescription> attachments;
     std::vector<vk::SubpassDescription> subpasses;
     std::vector<vk::SubpassDependency> dependencies;
 
-    const std::vector<vk::AttachmentReference> input_attachments;
-    const std::vector<vk::AttachmentReference> color_attachments;
-    const std::vector<vk::AttachmentReference> resolve_attachments;
-    const std::vector<vk::AttachmentReference> depth_stencil_attachment;
-    const std::vector<uint32_t> preserve_attachments;
+    std::vector<vk::AttachmentReference> input_attachments;
+    std::vector<vk::AttachmentReference> color_attachments;
+    std::vector<vk::AttachmentReference> resolve_attachments;
+    std::vector<uint32_t> subpass_resolve_attachment_count;
+    std::vector<vk::AttachmentReference> depth_stencil_attachment;
+    std::vector<uint32_t> preserve_attachments;
 };
 
 } // namespace merian
