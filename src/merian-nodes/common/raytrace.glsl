bb0bb0d4a508dfa7f729885b570e9d8bdee9e771
Lucas Alber <lucasd.alber@gmail.com>
2024-08-01 12:27:54 +0200

    merian-nodes: common: raytrace: Add macros for rayQuery stuff

--- src/merian-nodes/common/raytrace.glsl
+++ src/merian-nodes/common/raytrace.glsl
@@ -1,6 +1,21 @@
 #ifndef _RAYTRACE_H_
 #define _RAYTRACE_H_
 
+#define _rq_get_t(ray_query, commited) rayQueryGetIntersectionTEXT(ray_query, commited)
+#define _rq_instance_id(ray_query, commited) rayQueryGetIntersectionInstanceIdEXT(ray_query, commited)
+#define _rq_primitive_index(ray_query, commited) rayQueryGetIntersectionPrimitiveIndexEXT(ray_query, commited)
+#define _rq_barycentrics(ray_query, commited) vec3(1.0 - dot(rayQueryGetIntersectionBarycentricsEXT(ray_query, commited), vec2(1)), rayQueryGetIntersectionBarycentricsEXT(ray_query, commited))
+
+#define rq_get_t(ray_query) _rq_get_t(ray_query, true)
+#define rq_instance_id(ray_query) _rq_instance_id(ray_query, true)
+#define rq_primitive_index(ray_query) _rq_primitive_index(ray_query, true)  
+#define rq_barycentrics(ray_query) _rq_barycentrics(ray_query, true)
+
+#define rq_get_t_uc(ray_query) _rq_get_t(ray_query, false)
+#define rq_instance_id_uc(ray_query) _rq_instance_id(ray_query, false)
+#define rq_primitive_index_uc(ray_query) _rq_primitive_index(ray_query, false)  
+#define rq_barycentrics_uc(ray_query) _rq_barycentrics(ray_query, false)
+
 struct IntersectionInfo {
     uint instance_id;
     // primitive index in the instance
@@ -11,7 +26,7 @@ struct IntersectionInfo {
 };
 
 // The the intersection info for a commited intersection
-void get_intersection_info_commited(const rayQueryEXT ray_query, inout IntersectionInfo info) {
+void get_intersection_info_commited(const rayQueryEXT ray_query, out IntersectionInfo info) {
     info.instance_id = rayQueryGetIntersectionInstanceIdEXT(ray_query, true);
     info.primitive_index = rayQueryGetIntersectionPrimitiveIndexEXT(ray_query, true);
     info.barycentrics.yz = rayQueryGetIntersectionBarycentricsEXT(ray_query, true);
@@ -20,7 +35,7 @@ void get_intersection_info_commited(const rayQueryEXT ray_query, inout Intersect
 }
 
 // The the intersection info for an uncommited intersection
-void get_intersection_info_uncommited(const rayQueryEXT ray_query, inout IntersectionInfo info) {
+void get_intersection_info_uncommited(const rayQueryEXT ray_query, out IntersectionInfo info) {
     info.instance_id = rayQueryGetIntersectionInstanceIdEXT(ray_query, false);
     info.primitive_index = rayQueryGetIntersectionPrimitiveIndexEXT(ray_query, false);
     info.barycentrics.yz = rayQueryGetIntersectionBarycentricsEXT(ray_query, false);
