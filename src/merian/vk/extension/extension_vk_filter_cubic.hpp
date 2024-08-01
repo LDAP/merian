5949f49a6dbcb211707adb696b92fd4b1642de20
Lucas Alber <lucasd.alber@gmail.com>
2024-08-01 12:28:21 +0200

    merian: Add ExtensionVkFilterCubic

---
+++ src/merian/vk/extension/extension_vk_filter_cubic.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "merian/vk/extension/extension.hpp"
+
+namespace merian {
+
+class ExtensionVkFilterCubic : public Extension {
+  public:
+    ExtensionVkFilterCubic() : Extension("ExtensionVkFilterCubic") {}
+    ~ExtensionVkFilterCubic() {}
+    std::vector<const char*> required_device_extension_names(vk::PhysicalDevice) const override {
+        return {VK_EXT_FILTER_CUBIC_EXTENSION_NAME};
+    }
+};
+
+} // namespace merian
