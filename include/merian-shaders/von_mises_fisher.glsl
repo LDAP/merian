4bf3a0e58bf27d116c2259b095fb2c35ffade35d
Lucas Alber <lucasd.alber@gmail.com>
2024-12-12 13:13:53 +0100

    merian-shader: vmf: precompute constants

--- include/merian-shaders/von_mises_fisher.glsl
+++ include/merian-shaders/von_mises_fisher.glsl
@@ -8,14 +8,14 @@
 
 // numerically robust von Mises Fisher lobe
 float vmf_pdf(const float kappa, const float dotmu) {
-    if (kappa < 1e-4) return 1.0 / (4.0 * M_PI);
-    return kappa / (2.0 * M_PI * (1.0 - exp(-2.0 * kappa))) * exp(kappa * (dotmu - 1.0));
+    if (kappa < 1e-4) return INV_FOUR_PI;
+    return kappa / (TWO_PI * (1.0 - exp(-2.0 * kappa))) * exp(kappa * (dotmu - 1.0));
 }
 
 // see wenzel's doc on numerically stable expression for vmm
 vec3 vmf_sample(const float kappa, const vec2 random) {
-    const float w = 1.0 + log(random.x + (1.0-random.x) * exp(-2.0 * kappa)) / kappa;
-    const vec2 v = vec2(sin(2.0 * M_PI * random.y), cos(2.0 * M_PI * random.y));
+    const float w = 1.0 + log(random.x + (1.0 - random.x) * exp(-2.0 * kappa)) / kappa;
+    const vec2 v = vec2(sin(TWO_PI * random.y), cos(TWO_PI * random.y));
     return vec3(sqrt(1.0 - w * w) * v, w);
 }
 
@@ -26,7 +26,7 @@ vec3 vmf_sample(const vec3 z, const float kappa, const vec2 random) {
 
 // compute concentration parameter for given maximum density x
 float vmf_get_kappa(const float pdf) {
-    if (pdf > 0.795) return 2.0 * M_PI * pdf;
+    if (pdf > 0.795) return TWO_PI * pdf;
     return max(1e-5, (168.479 * pdf * pdf + 16.4585 * pdf - 2.39942) / (-1.12718 * pdf * pdf + 29.1433 * pdf + 1.0));
 }
 
