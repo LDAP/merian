1af019d5f551fa0fb07028ce8de69ce0e0b4ae4d
Lucas Alber <lucasd.alber@gmail.com>
2025-01-31 09:22:39 +0100

    wip

--- include/merian-nodes/graph/graph_run.hpp
+++ include/merian-nodes/graph/graph_run.hpp
@@ -22,6 +22,8 @@ class GraphRun {
   public:
     GraphRun(const uint32_t iterations_in_flight) : iterations_in_flight(iterations_in_flight) {}
 
+    // Enqueues a wait semaphore for the next submit. Note that during a graph run multiple submits
+    // might happen.
     void add_wait_semaphore(const BinarySemaphoreHandle& wait_semaphore,
                             const vk::PipelineStageFlags& wait_stage_flags) noexcept {
         wait_semaphores.push_back(*wait_semaphore);
@@ -29,11 +31,15 @@ class GraphRun {
         wait_values.push_back(0);
     }
 
+    // Enqueues a signal semaphore for the next submit. Note that during a graph run multiple
+    // submits might happen.
     void add_signal_semaphore(const BinarySemaphoreHandle& signal_semaphore) noexcept {
         signal_semaphores.push_back(*signal_semaphore);
         signal_values.push_back(0);
     }
 
+    // Enqueues a wait semaphore for the next submit. Note that during a graph run multiple submits
+    // might happen.
     void add_wait_semaphore(const TimelineSemaphoreHandle& wait_semaphore,
                             const vk::PipelineStageFlags& wait_stage_flags,
                             const uint64_t value) noexcept {
@@ -42,20 +48,22 @@ class GraphRun {
         wait_values.push_back(value);
     }
 
+    // Enqueues a signal semaphore for the next submit. Note that during a graph run multiple
+    // submits might happen.
     void add_signal_semaphore(const TimelineSemaphoreHandle& signal_semaphore,
                               const uint64_t value) noexcept {
         signal_semaphores.push_back(*signal_semaphore);
         signal_values.push_back(value);
     }
 
+    // Enqueues a callback that is executed after the next submit. Note that during a graph run
+    // multiple submits might happen.
     void add_submit_callback(
         const std::function<void(const QueueHandle& queue, GraphRun& run)>& callback) noexcept {
         submit_callbacks.push_back(callback);
     }
 
-    void request_reconnect() noexcept {
-        needs_reconnect = true;
-    }
+    // ------------------------------------------------------------------------------------
 
     // Number of iterations since connect.
     // Use get_total_iteration() for iterations since graph initialization.
@@ -82,56 +90,6 @@ class GraphRun {
     // returns the number of iterations that might be in flight at a certain time.
     const uint32_t& get_iterations_in_flight() const noexcept {
         return iterations_in_flight;
-    }
-
-    const CommandPoolHandle& get_cmd_pool() noexcept {
-        return cmd_pool;
-    }
-
-    // Add this to the submit call for the graph command buffer
-    const std::vector<vk::Semaphore>& get_wait_semaphores() const noexcept {
-        return wait_semaphores;
-    }
-
-    // Add this to the submit call for the graph command buffer
-    const std::vector<vk::PipelineStageFlags>& get_wait_stages() const noexcept {
-        return wait_stages;
-    }
-
-    // Add this to the submit call for the graph command buffer
-    const std::vector<vk::Semaphore>& get_signal_semaphores() const noexcept {
-        return signal_semaphores;
-    }
-
-    // Add this to the submit call for the graph command buffer
-    // The retuned pointer is valid until the next call to run.
-    vk::TimelineSemaphoreSubmitInfo get_timeline_semaphore_submit_info() const noexcept {
-        return vk::TimelineSemaphoreSubmitInfo{wait_values, signal_values};
-    }
-
-    // You must call every callback after you submited the graph command buffer
-    // Or you use the execute_callbacks function.
-    const std::vector<std::function<void(const QueueHandle& queue, GraphRun& run)>>&
-    get_submit_callbacks() const noexcept {
-        return submit_callbacks;
-    }
-
-    // Call this after you submitted the graph command buffer
-    void execute_callbacks(const QueueHandle& queue) {
-        for (const auto& callback : submit_callbacks) {
-            callback(queue, *this);
-        }
-    }
-
-    // Returns the profiler that is attached to this run.
-    //
-    // Can be nullptr if profiling is disabled!
-    const ProfilerHandle& get_profiler() const {
-        return profiler;
-    }
-
-    const ResourceAllocatorHandle& get_allocator() const {
-        return allocator;
     }
 
     // Returns the time difference to the last run in seconds.
@@ -166,10 +124,23 @@ class GraphRun {
         return to_seconds(elapsed_since_connect);
     }
 
-    // Hint the graph that waiting was necessary for external events. This information can be used
-    // to shift CPU processing back to reduce waiting and reduce latency.
-    void hint_external_wait_time(auto chrono_duration) {
-        external_wait_time = std::max(external_wait_time, chrono_duration);
+    // ------------------------------------------------------------------------------------
+
+    // the command pool for the current run. You should not need to use this directly. Use the sync,
+    // async, methods instead.
+    const CommandPoolHandle& get_cmd_pool() noexcept {
+        return cmd_pool;
+    }
+
+    // Returns the profiler that is attached to this run.
+    //
+    // Can be nullptr if profiling is disabled!
+    const ProfilerHandle& get_profiler() const {
+        return profiler;
+    }
+
+    const ResourceAllocatorHandle& get_allocator() const {
+        return allocator;
     }
 
     const ThreadPoolHandle& get_thread_pool() const {
@@ -178,6 +149,75 @@ class GraphRun {
 
     const CPUQueueHandle& get_cpu_queue() const {
         return cpu_queue;
+    }
+
+    // // Add this to the submit call for the graph command buffer
+    // const std::vector<vk::Semaphore>& get_wait_semaphores() const noexcept {
+    //     return wait_semaphores;
+    // }
+
+    // // Add this to the submit call for the graph command buffer
+    // const std::vector<vk::PipelineStageFlags>& get_wait_stages() const noexcept {
+    //     return wait_stages;
+    // }
+
+    // // Add this to the submit call for the graph command buffer
+    // const std::vector<vk::Semaphore>& get_signal_semaphores() const noexcept {
+    //     return signal_semaphores;
+    // }
+
+    // // Add this to the submit call for the graph command buffer
+    // // The retuned pointer is valid until the next call to run.
+    // vk::TimelineSemaphoreSubmitInfo get_timeline_semaphore_submit_info() const noexcept {
+    //     return vk::TimelineSemaphoreSubmitInfo{wait_values, signal_values};
+    // }
+
+    // // You must call every callback after you submited the graph command buffer
+    // // Or you use the execute_callbacks function.
+    // const std::vector<std::function<void(const QueueHandle& queue, GraphRun& run)>>&
+    // get_submit_callbacks() const noexcept {
+    //     return submit_callbacks;
+    // }
+
+    // // Call this after you submitted the graph command buffer
+    // void execute_callbacks(const QueueHandle& queue) {
+    //     for (const auto& callback : submit_callbacks) {
+    //         callback(queue, *this);
+    //     }
+    // }
+
+    // ------------------------------------------------------------------------------------
+    // Interact with graph runtime
+
+    // Hint the graph that waiting was necessary for external events. This information can be used
+    // to shift CPU processing back to reduce waiting and reduce latency.
+    void hint_external_wait_time(auto chrono_duration) {
+        external_wait_time = std::max(external_wait_time, chrono_duration);
+    }
+
+    void request_reconnect() noexcept {
+        needs_reconnect = true;
+    }
+
+    // ------------------------------------------------------------------------------------
+
+    const CommandBufferHandle& get_cmd() {
+        TODO
+    }
+
+    // ------------------------------------------------------------------------------------
+
+  private:
+
+    void begin_run(const CommandPoolHandle& cmd_pool) {}
+
+    /**
+     * @brief      Ends a run by submitting the last commandbuffer to the GPU.
+     *
+     * @param[in]  fence  The fence to signal when the submitted work completes.
+     */
+    void end_run(const vk::Fence& fence) {
+
     }
 
   private:
