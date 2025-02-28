2647673ba22d6f63220f4fc10aed40e5e72297ae
Lucas Alber <lucasd.alber@gmail.com>
2025-02-28 15:27:21 +0100

    merian-shaders: vMF: Add Tokuyoshis numerically stable implementation

--- include/merian-shaders/common.glsl
+++ include/merian-shaders/common.glsl
@@ -9,6 +9,8 @@
 #define INV_FOUR_PI 0.07957747154594767
 #define INV_SQRT_TWO_PI 0.3989422804014327
 
+#define FLT_EPSILON 1.192092896e-07F
+
 // returns 1/x if x > 0 else 1.
 #define MERIAN_SAFE_RECIPROCAL(x) (x > 0. ? 1. / x : 1.0)
 
@@ -25,6 +27,45 @@ bool merian_relative_distance_greather_than(const vec3 center, const vec3 p1, co
     const float d1 = distance(center, p1);
     const float d2 = distance(center, p2);
     return abs(d1 - d2) / max(d1, d2) > threshold;
+}
+
+// log(x + 1); David Goldberg (1991). What every computer scientist should know about floating-point arithmetic.
+float log1p (const float x) {
+    volatile const float u = x + 1.0f;
+    if (u == 1.0f) {
+        return x;
+    }
+    const float y = log(u);
+    if (x < 1.0f) {
+        return x * y / (u - 1.0f);
+    }
+    return y;
+}
+
+// exp(x) - 1; Nicholas J. Higham (2002). Accuracy and Stability of Numerical Algorithms. Society for Industrial and Applied Mathematics
+float expm1(const float x) {
+    const float u = exp(x);
+    if (u == 1.0f) {
+        return x;
+    }
+    const float y = u - 1.0f;
+    if (abs(x) < 1.0f) {
+        return x * y / log(u);
+    }
+    return y;
+}
+
+// x / (exp(x) - 1); Nicholas J. Higham (2002). Accuracy and Stability of Numerical Algorithms. Society for Industrial and Applied Mathematics
+float x_over_expm1(const float x) {
+    const float u = exp(x);
+    if (u == 1.0f) {
+        return 1.0f;
+    }
+    const float y = u - 1.0f;
+    if (abs(x) < 1.0f) {
+        return log(u) / y;
+    }
+    return x / y;
 }
 
 #endif // _MERIAN_SHADERS_COMMON_H_
