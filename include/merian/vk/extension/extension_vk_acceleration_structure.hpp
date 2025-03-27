7528c17e26a4098dc5cb2e9341fcbdba8d8068ff
Lucas Alber <lucasd.alber@gmail.com>
2025-03-27 10:39:04 +0100

    merian: ExtensionVkAccelerationStructure: Make features configurable

--- include/merian/vk/extension/extension_vk_acceleration_structure.hpp
+++ include/merian/vk/extension/extension_vk_acceleration_structure.hpp
@@ -12,48 +12,39 @@ namespace merian {
  */
 class ExtensionVkAccelerationStructure : public Extension {
   public:
-    ExtensionVkAccelerationStructure() : Extension("ExtensionVkAccelerationStructure") {}
-    ~ExtensionVkAccelerationStructure() {}
+    ExtensionVkAccelerationStructure(
+        const std::set<std::string>& required_features = {"accelerationStructure"},
+        const std::set<std::string>& optional_features = {
+            "accelerationStructureCaptureReplay", "accelerationStructureIndirectBuild",
+            "accelerationStructureHostCommands",
+            "descriptorBindingAccelerationStructureUpdateAfterBind"});
+
+    ~ExtensionVkAccelerationStructure();
 
     std::vector<const char*>
-    required_device_extension_names(const vk::PhysicalDevice&) const override {
-        return {
-            VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
-            VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME,
-        };
-    }
+    required_device_extension_names(const vk::PhysicalDevice& /*unused*/) const override;
 
     // LIFECYCLE
 
-    void on_physical_device_selected(const PhysicalDevice& pd_container) override {
-        vk::PhysicalDeviceProperties2KHR props2;
-        props2.pNext = &acceleration_structure_properties;
-        pd_container.physical_device.getProperties2(&props2);
-    }
+    void on_physical_device_selected(const PhysicalDevice& pd_container) override;
 
-    void* pnext_get_features_2(void* const p_next) override {
-        acceleration_structure_features.setPNext(p_next);
-        return &acceleration_structure_features;
-    }
+    void* pnext_get_features_2(void* const p_next) override;
 
     bool extension_supported(const vk::Instance& /*unused*/,
                              const PhysicalDevice& /*unused*/,
                              const ExtensionContainer& /*unused*/,
-                             const QueueInfo& /*unused*/) override {
-        return acceleration_structure_features.accelerationStructure == VK_TRUE;
-    }
+                             const QueueInfo& /*unused*/) override;
 
-    void* pnext_device_create_info(void* const p_next) override {
-        acceleration_structure_features.pNext = p_next;
-        return &acceleration_structure_features;
-    }
+    void* pnext_device_create_info(void* const p_next) override;
 
-    const uint32_t& min_scratch_alignment() const {
-        return acceleration_structure_properties.minAccelerationStructureScratchOffsetAlignment;
-    }
+    const uint32_t& min_scratch_alignment() const;
 
   private:
-    vk::PhysicalDeviceAccelerationStructureFeaturesKHR acceleration_structure_features;
+    std::set<std::string> required_features;
+    std::set<std::string> optional_features;
+
+    vk::PhysicalDeviceAccelerationStructureFeaturesKHR supported_acceleration_structure_features;
+    vk::PhysicalDeviceAccelerationStructureFeaturesKHR enabled_acceleration_structure_features;
 
   public:
     vk::PhysicalDeviceAccelerationStructurePropertiesKHR acceleration_structure_properties;
