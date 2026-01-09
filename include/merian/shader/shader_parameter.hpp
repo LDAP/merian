dcc832acda2272c48d8f4ea8c63f61689252c6bd
Lucas Alber <lucasd.alber@gmail.com>
2026-01-09 15:27:13 +0100

    wip

--- include/merian/shader/shader_parameter.hpp
+++ include/merian/shader/shader_parameter.hpp
@@ -1,17 +1,79 @@
 #pragma once
 
-// see
-// https://docs.shader-slang.org/en/latest/shader-cursors.html#making-a-multi-platform-shader-cursor
-// this is the ShaderCursor from Slangs docs.
-
-#include "merian/shader/shader_parameter_block.hpp"
-
+#include "merian/vk/descriptors/descriptor_container.hpp"
+#include "merian/vk/memory/resource_allocations.hpp"
 #include "slang.h"
+
+#include <cstddef>
+#include <cstdint>
 
 namespace merian {
 
+struct BindingOffset {
+    uint32_t binding_set;
+    uint32_t binding;
+    // uint32_t array_element;
+};
+
+struct ShaderOffset {
+    std::size_t uniform_byte_offset = 0;
+    uint32_t binding_range_offset = 0;
+    uint32_t binding_array_index = 0;
+};
+
+// see
+// https://docs.shader-slang.org/en/latest/shader-cursors.html#making-a-multi-platform-shader-cursor
+//
+// this is the ShaderObject in slang documentation.
+//
+// It holds the buffer and descriptor set for one feature, i.e a ParameterBlock<...> or the default
+// toplevel "block" and the target specific functions to write into it.
+class ParameterBlock {
+  public:
+    virtual void write(const ShaderOffset& offset, const ImageHandle& image) = 0;
+
+    virtual void write(const ShaderOffset& offset, const BufferHandle& buffer) = 0;
+
+    virtual void write(const ShaderOffset& offset, const TextureHandle& texture) = 0;
+
+    virtual void write(const ShaderOffset& offset, const SamplerHandle& sampler) = 0;
+
+    virtual void write(const ShaderOffset& offset, const void* data, std::size_t size) = 0;
+
+    template <class T> void write(const ShaderOffset& offset, const T& data) {
+        write(offset, &data, sizeof(T));
+    }
+};
+
+using ParameterBlockHandle = std::shared_ptr<ParameterBlock>;
+
+class DescriptorContainerParameterBlock : public ParameterBlock {
+  public:
+    virtual void write(const ShaderOffset& offset, const ImageHandle& image) override {}
+
+    virtual void write(const ShaderOffset& offset, const BufferHandle& buffer) override {}
+
+    virtual void write(const ShaderOffset& offset, const TextureHandle& texture) override {}
+
+    virtual void write(const ShaderOffset& offset, const SamplerHandle& sampler) override {}
+
+    virtual void write(const ShaderOffset& offset, const void* data, std::size_t size) override {}
+
+  private:
+    slang::TypeLayoutReflection* type_layout;
+
+    
+
+    BufferHandle constant_buffer;
+    DescriptorContainerHandle descriptor_container;
+};
+
 // Points to a position in a shader. Positions can be structs, fields (ordinary data, opaque data),
-// array (elemenets).
+// array (elemenets). Always relative to the corresponding parameter block this cursor refers to.
+//
+// see
+// https://docs.shader-slang.org/en/latest/shader-cursors.html#making-a-multi-platform-shader-cursor
+// this is the ShaderCursor from Slangs docs.
 class ShaderCursor {
   public:
     // --------------------------------------------------------------------
@@ -116,10 +178,24 @@ class ShaderCursor {
     }
 
   private:
-    ShaderParameterBlock* parameter_block = nullptr;
+    ParameterBlockHandle parameter_block = nullptr;
 
     slang::TypeLayoutReflection* type_layout;
     ShaderOffset offset;
+};
+
+// This represents a Slang-like struct that is bound to a shader either directly to a ParameterBlock
+// or as member of another ShaderObject.
+class ShaderObject {
+  public:
+    // Binds this object to the parameter block and starts updating the parameter block whenever
+    // this object changes.
+    void bind(ParameterBlock& block, ShaderCursor& cursor);
+
+  private:
+    // all the parameter blocks this object is currently bound to as well as a cursor that points to
+    // the object.
+    std::vector<std::pair<ParameterBlockHandle, ShaderCursor>> bindings;
 };
 
 } // namespace merian
