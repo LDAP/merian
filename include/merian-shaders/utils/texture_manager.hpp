39fcdb2e8b1dd7c1612633d39bb9ec84a985bc32
Lucas Alber <lucasd.alber@gmail.com>
2026-01-12 18:47:20 +0100

    wip

---
+++ include/merian-shaders/utils/texture_manager.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <memory>
+
+namespace merian {
+
+class TextureManager {};
+
+using TextureManagerHandle = std::shared_ptr<TextureManager>;
+
+} // namespace merian
--- include/merian/shader/shader_parameter.hpp
+++ include/merian/shader/shader_parameter.hpp
@@ -9,10 +9,35 @@
 
 namespace merian {
 
+/*
+ * - Nest as value: Cannot be reused -> object is only used as part of the outer struct (e.g.
+ * just for logical aggregation but not sharing) and cannot be shared between structs or shaders.
+ * Member variable on Host. The same object will never be also bound as constant buffer or parameter
+ * block.
+ * => The nested object does not need to be smart about making sure descriptor and buffer updates
+ * are executed, its the outer objects reponsibility. Also at max just one cursor (the outer one) is
+ * needed since its position is fixed.
+ *
+ * - Nest as constant buffer: The constant buffer can be reused (and thus has to be updated only
+ * once). For a small reused object that does not need its own descriptor set and is used only as
+ * part of the outer struct in the shader (struct of constants about something).
+ *
+ * - Nest as parameter block: The whole object might be reused at other places and always looks the
+ * same in the shader (same struct definition, same descriptor set and layout can be used). It is
+ * independent of the outer object. Has its own constant buffer and descriptor set (index). For
+ * example TextureManager, Scene.
+ * => The nested object does not need any reference to the outer object since it records its own
+ * updates and only when binding (the set) to the actual shader the outer object calls into the
+ * nested one to do so.
+ *
+ */
+
 struct BindingOffset {
     uint32_t binding_set;
     uint32_t binding;
     // uint32_t array_element;
+
+    uint32_t push_constant_range = 0;
 };
 
 struct ShaderOffset {
@@ -62,9 +87,6 @@ class DescriptorContainerParameterBlock : public ParameterBlock {
   private:
     slang::TypeLayoutReflection* type_layout;
 
-    
-
-    BufferHandle constant_buffer;
     DescriptorContainerHandle descriptor_container;
 };
 
@@ -184,18 +206,39 @@ class ShaderCursor {
     ShaderOffset offset;
 };
 
-// This represents a Slang-like struct that is bound to a shader either directly to a ParameterBlock
-// or as member of another ShaderObject.
-class ShaderObject {
-  public:
-    // Binds this object to the parameter block and starts updating the parameter block whenever
-    // this object changes.
-    void bind(ParameterBlock& block, ShaderCursor& cursor);
+class IParameterBlock {
 
   private:
-    // all the parameter blocks this object is currently bound to as well as a cursor that points to
-    // the object.
-    std::vector<std::pair<ParameterBlockHandle, ShaderCursor>> bindings;
+    DescriptorContainerHandle descriptors;
+};
+
+struct ShaderObjectLayout {
+    DescriptorSetLayoutHandle descriptor_set_layout;
+    std::size_t ordinary_buffer_size;
+};
+
+// This represents a Slang-like struct that is bound to a shader either directly to a ParameterBlock
+// or as member of another ShaderObject as value or ConstantBuffer.
+class ShaderObject {
+  public:
+    void bind_as_value(ShaderCursor& cursor);
+
+    void bind_as_constant_buffer(ShaderCursor& cursor);
+
+    void bind_as_parameter_pack(ShaderCursor& cursor);
+
+  private:
+    // Contains the ordinary data of this object and all objects that are value members of this
+    // object.
+    //
+    // Can be nullptr if this object was only bound as value to parents (then their ordinary data
+    // buffer is used) Do not write to this buffer directly but use the cursor in the binding
+    // instead.
+    BufferHandle ordinary_data = nullptr;
+
+    // the cursors pointing to the offsets of this object in parameter blocks this object is bound
+    // to.
+    std::vector<ShaderCursor> bindings;
 };
 
 } // namespace merian
