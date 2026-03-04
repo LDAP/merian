13d58ddb9662d6925b1f917283b387f7c61690a5
Lucas Alber <lucasd.alber@gmail.com>
2026-03-04 15:20:07 +0100

    wip

--- include/merian-nodes/nodes/svgf/svgf.hpp
+++ include/merian-nodes/nodes/svgf/svgf.hpp
@@ -62,9 +62,9 @@ class SVGF : public Node {
     std::optional<vk::Format> output_format = std::nullopt;
 
     // depends on available shared memory
-    const uint32_t variance_estimate_local_size = 32;
-    const uint32_t filter_local_size = 16;
-    const uint32_t taa_local_size = 16;
+    uint32_t variance_estimate_local_size;
+    uint32_t filter_local_size;
+    uint32_t taa_local_size;
 
     VkSampledImageInHandle con_prev_out = VkSampledImageIn::compute_read(1);
     VkSampledImageInHandle con_src = VkSampledImageIn::compute_read();
